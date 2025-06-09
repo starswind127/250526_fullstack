5de13bac202fc38dda62a93d45d173f575dbb1fe
starswind127 <kanzen127@naver.com>
2025-06-09 16:20:31 +0900

    0609 0420

---
+++ day09/실습2.js
@@ -0,0 +1,29 @@
+let 이름 = prompt("이름:");
+let 음료 = prompt("원하는 음료:");
+let 수량 = prompt("주문 수량:");
+
+console.log("[주문 접수]");
+console.log("이름");
+console.log(이름);
+console.log("메뉴");
+console.log(음료);
+console.log("수량");
+console.log(수량);
+
+let 다음 = prompt("다음")
+
+let 사용자이름 = prompt("이름:");
+let 운동종류 = prompt("운동 종류:");
+let 운동시간 = prompt("운동 시간:");
+
+const 칼로리 = 운동시간 * 7;
+
+console.log("[운동 기록]");
+console.log("이름");
+console.log(사용자이름);
+console.log("운동종류");
+console.log(운동종류);
+console.log("운동시간");
+console.log(운동시간);
+console.log("칼로리");
+console.log(칼로리);
