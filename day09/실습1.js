1f4a16cbbb0e16dae975ca419051f800f22f1ddd
starswind127 <kanzen127@naver.com>
2025-06-09 15:53:42 +0900

    0609 0353

---
+++ day09/실습1.js
@@ -0,0 +1,19 @@
+console.log("숫자자료")
+console.log("1. 대기번호")
+console.log("2. 화면전환번호")
+console.log("3. 주문수량")
+
+console.log("문자열자료")
+console.log("1. 카테고리명")
+console.log("2. 지점명")
+console.log("3. 쿠폰번호")
+
+console.log("불리언자료")
+console.log("1. 영수증발급여부")
+console.log("2. 얼음선택")
+console.log("3. 쿠폰사용여부")
+
+console.log("배열자료")
+console.log("1. 유료옵션")
+console.log("2. 카테고리내음료")
+console.log("3. 카테고리종류")
