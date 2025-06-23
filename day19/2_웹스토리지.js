a5854de8ae4e91a54b32fb07d0168432be601ef9
starswind127 <kanzen127@naver.com>
2025-06-23 12:23:02 +0900

    0623 1222

--- day19/2_웹스토리지.js
+++ day19/2_웹스토리지.js
@@ -8,19 +8,42 @@ function func1(){
     const user = userInput.value;
         console.log( user );
     localStorage.setItem('user', user );
+        console.log( localStorage );
+
 }
 
 // 예제 2: localStorage에 객체 저장하기 (JSON.stringify)
 // 아래 사용자의 환경 설정(객체)을 문자열로 변환하여 localStorage에 저장 하시오. 
 // const userSettings = { theme: 'dark', notifications: true, language: 'ko' };
+const userSettings = { theme: 'dark', notifications: true, language: 'ko' };
+let data = JSON.stringify( userSettings );
+localStorage.setItem('data',data);
+console.log(localStorage);
 
 // 예제 3: localStorage에서 객체 불러오기 (JSON.parse)
 // localStorage에 문자열로 저장된 사용자 설정(userSettings) 을 다시 객체로 변환하여 사용 하시오. 
 
+let getDate = localStorage.getItem('data');
+let jsonData = JSON.parse(gerData);
+console.log( jsonData );
+
 // 예제 4: 데이터 삭제하기 (removeItem, clear)
 // 스토리지에 저장된 특정 데이터(userSettings) 또는 전체 데이터를 삭제 하시오. 
+localStorage.removeItem('data');
+localStorage.clear();
 
 // 예제 5: 방문 횟수 카운터 만들기
 // 페이지가 로드될 때마다 sessionStorage를 사용하여 방문 횟수를 1씩 증가시키시오. 
 // 현재 방문 횟수를 id="visitCount"인 span 태그에 "이번 세션에서 O번째 방문입니다." 형식으로 출력하시오. 
 // (페이지를 새로고침하면 숫자가 올라가고, 브라우저 탭을 닫았다 다시 열면 1로 초기화되어야 합니다.)
+// 1. 기존의 웹스토리지 불러오기/확인 
+let visit = sessionStorage.getItem( 'visit' );    console.log( visit );   
+if( visit == null ){
+    sessionStorage.setItem( 'visit' , 1 );
+    visit = 1 ;   
+}else{
+    visit = Number( visit ) + 1 
+    sessionStorage.setItem( 'visit' , visit );
+}
+const visitCount = document.querySelector('#visitCount');
+visitCount.innerHTML = visit
